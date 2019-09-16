e1fcefccfa11a48456352f6978e138aca7ca8fc4
Robert <robert_jesusaz@outlook.com>
2019-09-16 19:15:24 -0300

    Subindo atualizacao na lista de venda

--- resources/views/sales/new.blade.php
+++ resources/views/sales/new.blade.php
@@ -20,7 +20,7 @@
             <button type="submit" class="btn btn-info" >Voltar</button>
         </a>
     </div>  
-    <form action="{{ route('sales.new') }}" method="post">
+    <form action="" method="post">
         {!! csrf_field() !!}
         <div class="form-row">
             <div class="form-group col-md-4 has-feedback {{$errors->has('cidade') ? 'has-error' : '' }}">
@@ -61,62 +61,83 @@
                 <input type="text" name="protocol" class="form-control" placeholder="Número do Comprovante..">
             </div>
         </div>
-            <div class="form-row">
-                <div class="form-group col-md-4 has-feedback {{$errors->has('cidade') ? 'has-error' : '' }}">
-                    <label for="inputCity">Produto</label>
-                    <select name="produto" class="form-control stateClient">
-                            <option></option>
-                        <?php if(empty($product) == null) { ?>
-                            <?php foreach($product as $data){ ?>
-                                <option>{{$data->description}}</option>
-                            <?php }?>
-                        <?php }?>
-                    </select>
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                    @if ($errors->has('cidade'))
-                        <span class="help-block">
-                            <strong class="error">{{ $errors->first('cidade') }}</strong>
-                        </span>
-                    @endif
-                </div>
-                <div class="form-group col-md-2 has-feedback {{$errors->has('cidade') ? 'has-error' : '' }}">      
-                    <label name="amount">Quantidade</label>
-                    <select name="amount" class="form-control" value="[]">
-                        <option></option>
-                    </select>
-                    
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                    @if ($errors->has('cidade'))
-                        <span class="help-block">
-                            <strong class="error">{{ $errors->first('cidade') }}</strong>
-                        </span>
-                    @endif
-                    </div>
-                <div class="form-group col-md-2 has-feedback {{$errors->has('cidade') ? 'has-error' : '' }}">
-                    <label for="inputCity">Preço Venda</label>
-                    <select name="priceOld" class="form-control" value="[]">
-                        <option></option>
-                    </select>
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                    @if ($errors->has('cidade'))
-                        <span class="help-block">
-                            <strong class="error">{{ $errors->first('cidade') }}</strong>
-                        </span>
-                    @endif
-                </div>
-                <div class="form-group col-md-2 has-feedback {{$errors->has('cidade') ? 'has-error' : '' }}">
-                    <label for="inputCity">Desconto</label>
-                    <input type="text" class="form-control">
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                    @if ($errors->has('cidade'))
-                        <span class="help-block">
-                            <strong class="error">{{ $errors->first('cidade') }}</strong>
-                        </span>
-                    @endif
-                </div>            
-        </div> 
-        <button type="submit" class="btn btn-success" >Adicionar</button>                 
     </form>
+    <form action="" method="post" id="products">
+        <div class="form-row">
+            <div class="form-group col-md-4 has-feedback {{$errors->has('cidade') ? 'has-error' : '' }}">
+                <label for="inputCity">Produto</label>
+                <select name="produto" class="form-control stateClient">
+                        <option></option>
+                    <?php if(empty($product) == null) { ?>
+                        <?php foreach($product as $data){ ?>
+                            <option>{{$data->description}}</option>
+                        <?php }?>
+                    <?php }?>
+                </select>
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                @if ($errors->has('cidade'))
+                    <span class="help-block">
+                        <strong class="error">{{ $errors->first('cidade') }}</strong>
+                    </span>
+                @endif
+            </div>
+            <div class="form-group col-md-2 has-feedback {{$errors->has('cidade') ? 'has-error' : '' }}">      
+                <label name="amount">Quantidade</label>
+                <select name="amount" class="form-control" value="[]">
+                    <option></option>
+                </select>
+                
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                @if ($errors->has('cidade'))
+                    <span class="help-block">
+                        <strong class="error">{{ $errors->first('cidade') }}</strong>
+                    </span>
+                @endif
+                </div>
+            <div class="form-group col-md-2 has-feedback {{$errors->has('cidade') ? 'has-error' : '' }}">
+                <label for="inputCity">Preço</label>
+                <select name="priceOld" class="form-control" value="[]">
+                    <option></option>
+                </select>
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                @if ($errors->has('cidade'))
+                    <span class="help-block">
+                        <strong class="error">{{ $errors->first('cidade') }}</strong>
+                    </span>
+                @endif
+            </div>
+            <div class="form-group col-md-2 has-feedback {{$errors->has('cidade') ? 'has-error' : '' }}">
+                <label for="inputCity">Desconto</label>
+                <input type="text" class="form-control">
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                @if ($errors->has('cidade'))
+                    <span class="help-block">
+                        <strong class="error">{{ $errors->first('cidade') }}</strong>
+                    </span>
+                @endif
+            </div>            
+        </div>
+        <button type="btn" class="btn btn-success" onclick="fechar_div();">Adicionar Produto</button> 
+    </form>
+        <button type="submit" class="btn btn-success">Adicionar</button>                 
+
+    <div class="row" id="filho" style="display:none;">
+        <div class="form-group col-md-12">
+            <h1>Produtos</h1><hr>
+            <table id="grid" class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Quantidade</th>
+                        <th>Preço</th>
+                    </tr>
+                </thead>
+                <tbody>
+                
+                </tbody>
+            </table>
+        </div>
+    </div>
     
 </div>
 @section('post-script')
@@ -132,6 +153,27 @@
             });
         });
     });
+
+    function fechar_div(){
+            var x = document.getElementById('filho');
+            if (x.style.display === 'none') {
+                x.style.display = 'block';
+            }
+        }
+
+    $(document).ready(function(){
+	    $('#products').submit(function(){
+		var $this = $(this) ;
+		var tr = '<tr>'+
+			'<td>'+$this.find("select[name='produto']").val()+'</td>'+
+			'<td>'+$this.find("select[name='amount']").val()+'</td>'+
+			'<td>'+$this.find("select[name='priceOld']").val()+'</td>'+
+			'</tr>'
+		$('#grid').find('tbody').append( tr );
+
+		return false;
+	});
+});
 </script>
 @endsection
 
