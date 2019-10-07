59d3a6622744b14c3df93e46852a2f00fedb331c
Gustavo <gustavoz.longo@gmail.com>
2019-10-07 13:07:20 -0300

    subindo vendas e alterações na welcome

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -3,5 +3,59 @@
 @section('title', 'Software para Gestão de Oficina')
 
 @section('content')
+<div class="register-box-body">
+    <p class="login-box-msg">Vendas</p>
+    @if(session('success'))
+        <div class="alert alert-info">
+            {{ session('success') }}
+        </div>
+    @endif
 
+    @if(session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
+    <div class="form-row">
+        <p class="login-box-msg">Lista de Vendas</p>
+        <a href="{{route('sales.new')}}">
+        <button type="button" class="btn btn-success float-left" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Nova Venda</button><br><br>
+        </a>
+        
+    </div><hr>
+    <div class="form-row">
+        <div class="pai">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Data</th>
+                        <th scope="col">Cliente</th>
+                        <th scope="col">Tipo Comprovante</th>
+                        <th scope="col">Taxa</th>
+                        <th scope="col">Total</th>
+                        <th scope="col">Estado</th>
+                        <th scope="col">Opções</th>
+                    </tr>
+                </thead>
+                
+                <?php if(empty($list) == null){ ?>
+                    <?php foreach($list as $sales){ ?>
+                        <tr>
+                            <td>{{$client->data}}</td>
+                            <td>{{$client->client}}</td>
+                            <td>{{$client->vouchertype}}</td>
+                            <td>{{$client->rate}}</td>
+                            <td>{{$client->total }}</td>
+                            <td>{{$client->state}}</td>
+                            <td><a href="{{ route('sales.view', $client->id)}}" class="text-danger"><i class="fa fa-file-text-o"></i></a></td>
+                            <td><a href="{{ route('sales.destroy', $client->id)}}" class="text-danger" onclick="return confirm('Tem certeza que deseja deletar esta?')"><i class="fa fa-trash"></i></a></td>
+                        </tr>
+                    <?php } ?>
+                <?php } ?>
+            </table>
+        </div>
+    </div>
+        
+    
+</div>
 @stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>SG</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -73,7 +73,7 @@
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">Registrar</a>
                         @endif
                     @endauth
                 </div>
@@ -81,17 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Software Gestão de Oficina
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{route('login')}}">Administrador</a>
+                    <a href="{{route('login')}}">Mecânico</a>
+                    
                 </div>
             </div>
         </div>
